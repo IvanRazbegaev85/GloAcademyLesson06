85d6c926040780cf42cd4dd62624d591b923696b
Ivan Razbegaev <sinnless13@gmail.com>
2021-10-08 02:14:56 +0300

    lesson 06 done

---
+++ script.js
@@ -0,0 +1,46 @@
+const random = function (num) {
+
+    const checkInput = function () {
+        let playersNumber = prompt("Введите число от 1 до 100:");
+
+        const numberInput = function (num) {                                //проверяем пользовательский ввод и если надо - приводим к числу
+            const isNumber = function (num) {
+                return !isNaN(parseFloat(num)) && isFinite(num);
+            };
+            if (isNumber(num)) {
+                playersNumber = parseFloat(num.trim());
+                return playersNumber;
+            } else return false;
+        };
+
+        const playersGuess = function () {                                  // Рекурсивно спрашиваем игрока число, если не равно загаданному
+                if (playersNumber < num){
+                    console.log("Загаданное число больше");
+                    checkInput();
+                } else if(playersNumber > num) {
+                    console.log("Загаданное число меньше");
+                    checkInput();
+                } else if (playersNumber !== playersNumber){
+                    console.log("Игра окончена")
+                    return;
+                } else {
+                    console.log("Поздравляю, Вы угадали!!!");
+                    return;
+                };
+            };
+            if (!playersNumber){                                            // Проверяем на false ввод пользователя
+                console.log("Игра окончена");
+            } else if (!numberInput(playersNumber)){                        // Проверяем на введено ли число, нет - алертим
+                alert("Введите число!");
+                checkInput();
+            } else playersGuess();                                          // Если все ок - уходим сравнивать число с загаданным
+
+    };
+    checkInput();                                                           // Запускаем проверку введенного
+
+};
+
+random((Math.random()*100).toFixed());
+
+
+
